4d91c829684018f364b10aaa0f914bc70203e095
Daniel Garcia <danielg_97@yahoo.com>
2021-01-17 03:42:45 -0500

    Updated contact page

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,17 +1,51 @@
 import React from 'react';
+import emailjs from 'emailjs-com'
 // import {motion} from 'framer-motion';
 import '../css/contact.css';
 import descriptions from "../json/descriptions.json";
 
+const learnMoreHeading = descriptions.contact.heading;
 const learnMoreDescription = descriptions.contact.description;
+
+const linkedinLink = "https://www.linkedin.com/in/itsdanielg/";
+const githubLink = "https://github.com/itsdanielg";
+const instagramLink = "https://www.instagram.com/itsdanielg_/";
+
+const linkedinIcon = "/svg/linkedin.svg";
+const githubIcon = "/svg/github.svg";
+const instagramIcon = "/svg/instagram.svg";
+
+const serviceID = 'service_0v89qij';
+const templateID = 'default_template_id';
+const userID = 'user_gJB2xJxcjxHaAsGivS4G1';
 
 function Contact() {
 
-    var onSubmit = () => {
-        var name = document.getElementById("contact-input-name").value;
-        var email = document.getElementById("contact-input-email").value;
-        var message = document.getElementById("contact-input-message").value;
-        console.log([name, email, message]);
+    var sendEmail = (e) => {
+        e.preventDefault();
+        var message = document.getElementById("contactSentMessage");
+        emailjs.sendForm(serviceID, templateID, e.target, userID)
+        .then((success) => {
+            message.innerHTML = "Your message has been sent!"
+            message.style.color = "var(--green)";
+            clearForm();
+            console.log(success.text);
+        }, (error) => {
+            message.innerHTML = "Your message could not be sent!"
+            message.style.color = "var(--red)";
+            console.log(error.text);
+        });
+        message.classList.remove("contact-sent");
+        setTimeout(() => {
+            message.classList.add("contact-sent");
+        }, 1);
+    }
+
+    var clearForm = () => {
+        document.getElementById("contact-input-name").value = "";
+        document.getElementById("contact-input-email").value = "";
+        document.getElementById("contact-input-subject").value = "";
+        document.getElementById("contact-input-message").value = "";
     }
 
     return (
@@ -20,14 +54,34 @@ function Contact() {
                 <h1 className="title-heading">CONTACT</h1>
                 <div className='title-underline'></div>
             </div>
-            <div className="page-body" id="contentBody">
+            <div className="page-body" id="contact_body">
+                <h2>{learnMoreHeading}</h2>
                 <p>{learnMoreDescription}</p>
-                <form className="contact-form">
+                <div className="social-links-container">
+                    <div className="social-link-wrapper">
+                        <a href={linkedinLink} target="_blank" rel="noopener noreferrer">
+                            <img src={linkedinIcon} alt="LinkedIn Icon"></img>
+                        </a>
+                    </div>
+                    <div className="social-link-wrapper">
+                        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                            <img src={githubIcon} alt="Github Icon"></img>
+                        </a>
+                    </div>
+                    <div className="social-link-wrapper">
+                        <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                            <img src={instagramIcon} alt="Instagram Icon"></img>
+                        </a>
+                    </div>
+                </div>
+                <form className="contact-form" onSubmit={sendEmail}>
                     <input className="contact-input" id="contact-input-name" type="text" placeholder="Full Name" name="name"></input>
                     <input className="contact-input" id="contact-input-email" type="text" placeholder="Email" name="email"></input>
+                    <input className="contact-input" id="contact-input-subject" type="text" placeholder="Subject" name="subject"></input>
                     <textarea className="contact-input" id="contact-input-message" type="text" placeholder="Enter your message!" name="message"></textarea>
+                    <input className="contact-submit" type="submit" value="Submit"></input>
                 </form>
-                <div className="contact-submit" onClick={onSubmit}>Submit</div>
+                <p className="contact-sent" id="contactSentMessage"></p>
             </div>
         </div>
     );
