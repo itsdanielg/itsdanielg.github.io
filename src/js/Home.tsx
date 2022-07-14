097fb7b95b3ca0ae5ef32e9e87b99ccba512c236
Daniel Garcia <danielg_97@yahoo.com>
2022-07-14 17:54:59 -0400

    Home page typescript integration

--- src/js/Home.tsx
+++ src/js/Home.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
 import {Link} from "react-router-dom";
-// import {motion} from 'framer-motion';
-import '../css/home.css';
+import '../css/Home.css';
 
 const homeImage = "/images/home.png";
 
 function Home() {
 
-    var demoClick = () => {
-        console.log("opening demo");
-    }
-
     return (
         <div className="home-container">
             <div className="header-container">
                 <div className="text-container">
+                    <h3>Hey, what's up? 🙂</h3>
+                    <br></br>
                     <h1>Daniel Jedryl Garcia</h1>
                     <h2>Software Engineer • Game Programmer</h2>
                 </div>
                 <div className="button-container">
                     <Link to="/about">
-                        <button className="home-button" type="button">Learn More</button>
+                        <button className="home-button" type="button">Interactive</button>
                     </Link>
-                    <button className="home-button" type="button" onClick={demoClick}>View Demo</button>
+                    <button className="home-button" type="button">Compact</button>
                 </div>
             </div>
             <div className="home-image-container">
