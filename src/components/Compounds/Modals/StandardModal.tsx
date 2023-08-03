ef414c0e8fdf0c79e4dcff76ea1dc0a830a71799
itsdanielg <danielg_97@yahoo.com>
2023-08-03 02:13:13 -0400

    Refactored Modal

--- src/components/Compounds/Modals/StandardModal.tsx
+++ src/components/Compounds/Modals/StandardModal.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import { Modal } from "../../Atoms/Modal";
+import { Modal } from "../../Atoms";
 
 interface StandardModalProps {
   show: boolean;
