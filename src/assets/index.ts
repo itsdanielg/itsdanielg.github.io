36364165975ad22570cbb8e6358314e358acd377
itsdanielg <danielg_97@yahoo.com>
2023-09-08 00:46:34 -0400

    Refactored assets

---
+++ src/assets/index.ts
@@ -0,0 +1,79 @@
+import c4me from "./images/c4me.png";
+import city from "./images/city.png";
+import organ from "./images/organ.png";
+import portfolio from "./images/portfolio.png";
+import portrait from "./images/portrait.jpg";
+import portraitTransparent from "./images/portraitTransparent.png";
+import speedbot from "./images/speedbot.png";
+import spotifysorter from "./images/spotifysorter.png";
+
+import audacity from "./svg/audacity.svg";
+import blender from "./svg/blender.svg";
+import csharp from "./svg/csharp.svg";
+import css from "./svg/css.svg";
+import defmethod from "./svg/defmethod.svg";
+import email from "./svg/email.svg";
+import git from "./svg/git.svg";
+import github from "./svg/github.svg";
+import grid from "./svg/grid.svg";
+import html from "./svg/html.svg";
+import java from "./svg/java.svg";
+import javascript from "./svg/javascript.svg";
+import jquery from "./svg/jquery.svg";
+import linkedin from "./svg/linkedin.svg";
+import list from "./svg/list.svg";
+import nodejs from "./svg/nodejs.svg";
+import phaser from "./svg/phaser.svg";
+import photoshop from "./svg/photoshop.svg";
+import python from "./svg/python.svg";
+import react from "./svg/react.svg";
+import sbu from "./svg/sbu.svg";
+import sql from "./svg/sql.svg";
+import tailwindcss from "./svg/tailwindcss.svg";
+import typescript from "./svg/typescript.svg";
+import unity from "./svg/unity.svg";
+import vite from "./svg/vite.svg";
+import web from "./svg/web.svg";
+
+const PNG = {
+  c4me: c4me,
+  city: city,
+  organ: organ,
+  portfolio: portfolio,
+  portrait: portrait,
+  portraitTransparent: portraitTransparent,
+  speedbot: speedbot,
+  spotifysorter: spotifysorter
+};
+
+const SVG = {
+  audacity: audacity,
+  blender: blender,
+  csharp: csharp,
+  css: css,
+  defmethod: defmethod,
+  email: email,
+  git: git,
+  github: github,
+  grid: grid,
+  html: html,
+  java: java,
+  javascript: javascript,
+  jquery: jquery,
+  linkedin: linkedin,
+  list: list,
+  nodejs: nodejs,
+  phaser: phaser,
+  photoshop: photoshop,
+  python: python,
+  react: react,
+  sbu: sbu,
+  sql: sql,
+  tailwindcss: tailwindcss,
+  typescript: typescript,
+  unity: unity,
+  vite: vite,
+  web: web
+};
+
+export { PNG, SVG };
