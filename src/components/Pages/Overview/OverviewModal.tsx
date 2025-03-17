28dd3396bb19946b164ea2dcaf26156e4af9df55
itsdanielg <danielg_97@yahoo.com>
2025-03-17 17:51:51 -0400

    Refactored OverviewModal, changed dimensions

--- src/components/Pages/Overview/OverviewModal.tsx
+++ src/components/Pages/Overview/OverviewModal.tsx
@@ -9,51 +9,72 @@ import { Info } from "./Info";
 import { Projects } from "./Projects";
 import { TechStack } from "./TechStack";
 
+const experiencesToShow = [experiences[0]];
+const projectsToShow = [projects[0], projects[1], projects[3]];
+
 interface OverviewModalProps {
   show: boolean;
   setShow: Dispatch<SetStateAction<boolean>>;
 }
-
 export function OverviewModal({ show, setShow }: OverviewModalProps) {
-  const experiencesToShow = [experiences[0]];
-  const projectsToShow = [projects[0], projects[1], projects[3]];
-
   return (
     <StandardModal
-      className="flex flex-col w-full md:w-auto h-full md:h-auto overflow-hidden"
+      className="flex flex-col w-full md:w-[90%] h-full md:h-[90%] overflow-hidden"
       show={show}
       setShow={setShow}>
-      <div className="flex flex-col md:flex-row w-full h-full gap-12 md:gap-0 p-8 overflow-auto">
-        <div className="flex flex-col items-start gap-4 w-full md:w-80 md:pr-6 md:mr-6 md:border-r md:border-black">
-          <StaticImage asset="portrait" />
-          <Info />
-          <Education />
-        </div>
-        <div className="relative flex flex-col w-full items-start gap-12 md:gap-4">
-          <Experiences experiences={experiencesToShow} />
-          <Projects projects={projectsToShow} />
-          <TechStack />
-          <div className="hidden md:flex gap-4 absolute bottom-0 right-0">
-            <LinkButton
-              to="/projects"
-              animated>
-              View All Projects
-            </LinkButton>
-            <Contact />
-          </div>
-        </div>
+      <div className="flex w-full grow p-8 overflow-hidden">
+        <OverviewModal.LeftSideColumn />
+        <OverviewModal.RightSideColumn />
       </div>
-      <div className="md:hidden flex items-center w-full p-4 bg-white-1 border-t-2">
-        <Button onClick={() => setShow(false)}>Close</Button>
-        <div className="flex items-center gap-2 ml-auto">
-          <LinkButton
-            to="/projects"
-            animated>
-            View All Projects
-          </LinkButton>
-          <Contact />
-        </div>
-      </div>
+      <OverviewModal.MobileBottomBar setShow={setShow} />
     </StandardModal>
   );
 }
+
+function LeftSideColumn() {
+  return (
+    <div className="flex flex-col items-start gap-4 w-full md:w-80 md:pr-6 md:mr-6 md:border-r md:border-black overflow-y-auto">
+      <StaticImage asset="portrait" />
+      <Info />
+      <Education />
+    </div>
+  );
+}
+
+function RightSideColumn() {
+  return (
+    <div className="relative flex flex-col w-full items-start gap-12 md:gap-4 overflow-y-auto">
+      <Experiences experiences={experiencesToShow} />
+      <Projects projects={projectsToShow} />
+      <TechStack />
+      <div className="hidden md:flex md:ml-auto gap-4 bottom-0 right-0">
+        <LinkButton
+          to="/projects"
+          animated>
+          View All Projects
+        </LinkButton>
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+function MobileBottomBar({ setShow }: Pick<OverviewModalProps, "setShow">) {
+  return (
+    <div className="md:hidden flex items-center w-full p-4 bg-white-1 border-t-2">
+      <Button onClick={() => setShow(false)}>Close</Button>
+      <div className="flex items-center gap-2 ml-auto">
+        <LinkButton
+          to="/projects"
+          animated>
+          View All Projects
+        </LinkButton>
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+OverviewModal.LeftSideColumn = LeftSideColumn;
+OverviewModal.RightSideColumn = RightSideColumn;
+OverviewModal.MobileBottomBar = MobileBottomBar;
